98bbc332e7d1690661583e8a66b4458f80470199
Rajan Prajapati <rajprajapaticode85@gmail.com>
2026-09-05 13:59:25 +0530

    Update auth.api.jsx

--- frontend/src/features/auth/services/auth.api.jsx
+++ frontend/src/features/auth/services/auth.api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL:"http://localhost:3000",
+  baseURL:"https://ai-resume-analyzer-backend-67qa.onrender.com",
   withCredentials:true
 })
 
